18c36f1b065a2d1bf00c970bd0479ae1b3234eaa
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-07-27 17:42:01 -0300

    feat: adição de rotas com React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,16 @@
 import './App.css'
 
-import h2Logo from './assets/11459762.png';
-import swaggerLogo from './assets/swagger-icon-2048x2048-563qbzey.png';
-import githubLogo from "./assets/25231.png";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Index from './paginas/Index';
 
 function App() {
 
   return (
-    <>
-      <header>
-        <div>
-            <h1>Desafio DIO - 2024</h1>
-            <p>Spring Boot Bootcamp</p>
-        </div>
-      </header>
-
-      <main>
-        <div className="navegacao">
-            <ul>
-                <li>
-                    <span><img src={h2Logo}/></span>
-                    <span><a href="http://localhost:8080/h2-console">Acessar o H2 Database</a></span>
-                </li>
-                <li>
-                    <span><img src={swaggerLogo}/></span>
-                    <span><a href="http://localhost:8080/swagger-ui/index.html">Acessar a documentação Swagger</a></span>
-                </li>
-                <li>
-                    <span><img src={githubLogo}/></span>
-                    <span><a href="http://localhost:8080/https://github.com/NattanSouza404">Meu Github</a></span>
-                </li>
-            </ul>
-        </div>
-      </main>
-
-      <footer/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+      </Routes>
+    </Router>
   )
 }
 
